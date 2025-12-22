57cad9d8f7fd179d6c6c48f7390c1fec8fe9924e
Noushin Anika Khan <noushinanikakhan@gmail.com>
2025-12-22 21:14:32 +0600

    deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
 // src/utils/api.js
 export const API_BASE =
-  (import.meta.env.VITE_API_BASE || "http://localhost:3000").replace(/\/+$/, "");
+  import.meta.env.VITE_API_BASE || "https://civic-care-server.vercel.app"
